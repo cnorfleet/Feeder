7780015cb401280ed8bf0eec7fa1ee970b152005
KravitzLab <katrina.p.nguyen@gmail.com>
2016-06-07 17:08:15 -0400

    Add files via upload

---
+++ fed-arduino/RTC_code.ino
@@ -0,0 +1,17 @@
+// Date and time functions using a DS1307 RTC connected via I2C and Wire lib
+
+#include <Wire.h>
+#include "RTClib.h"
+
+RTC_DS1307 RTC;
+
+void setup () {
+    Serial.begin(57600);
+    Wire.begin();
+    RTC.begin();
+
+    RTC.adjust(DateTime(__DATE__, __TIME__));
+
+}
+
+void loop () {}
